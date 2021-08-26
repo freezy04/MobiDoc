bf5241eaa903b875787b0ad0e92046affa190eff
mpho <yungmpho@gmail.com>
2021-08-26 13:26:01 +0200

    Patient Complete appointment

--- Patient/scripts/CompletedAppointments.js
+++ Patient/scripts/CompletedAppointments.js
@@ -1,4 +1,31 @@
 
+function AddtoHtml(app,appList) {
+
+    let label;
+    label = "<span class='complete'>" + app.status + "</span>";
+    appList.innerHTML +=
+        "<div class='card' >"+
+        "<h2>Appointment with " + app.doctor_Name + "</h2>" +
+        "<p>" + app.date_for_appointment + " - " + app.time_for_appointment + "</p>" +
+        "<table>" +
+        "<tr>" +
+        "<th>Appointment notes: </th>" +
+        "<td>" + app.notes + "</td>" +
+        "</tr>" +
+        "<tr>" +
+        "<th>Consulation cost: </th>" +
+        "<td>" + app.appointment_Cost + "</td>" +
+        "</tr>" +
+        "<tr>" +
+        "<th>Reason for appointment: </th>" +
+        "<td>" + app.reason_for_appointment + "</td>" +
+        "</tr>" +
+        "</table>"
+        +"</div>"
+        +"<br/>"
+    ;
+}
+
 
 function updatemenu() {
     if (document.getElementById('responsive-menu').checked === true) {
@@ -10,4 +37,4 @@ function updatemenu() {
     }
 }
 
-module.exports = {updatemenu}
+module.exports = {AddtoHtml,updatemenu}
