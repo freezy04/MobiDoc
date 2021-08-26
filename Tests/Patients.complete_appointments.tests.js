9911b0711794e28d3f31e8e595e37bb60af76148
mpho <yungmpho@gmail.com>
2021-08-26 13:18:31 +0200

    CompletedAppointments unit test

---
+++ Tests/Patients.complete_appointments.tests.js
@@ -0,0 +1,22 @@
+const Appoint = require("../Patient/scripts/CompletedAppointments")
+
+
+test("Update Menu where the menu is responsive", () => {
+    // setting up the DOM elements
+    document.body.innerHTML = "<div id='responsive-menu'></div>" +
+        "<div id='menu'></div>"
+    document.getElementById('responsive-menu').checked = true;
+    Appoint.updatemenu();
+    expect(document.getElementById('menu').style.borderBottomRightRadius).toBe("0");
+
+});
+
+test("Update Menu where the menu is unresponsive", () => {
+    // setting up the DOM elements
+    document.body.innerHTML = "<div id='responsive-menu'></div>" +
+        "<div id='menu'></div>"
+    document.getElementById('responsive-menu').checked = false;
+    Appoint.updatemenu();
+    expect(document.getElementById('menu').style.borderRadius).toBe("0px");
+
+});
