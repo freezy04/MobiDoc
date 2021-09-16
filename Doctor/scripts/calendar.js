72dbc6eac29e57cb7866af3201211632ca454364
gabe <gabelancaster0@gmail.com>
2021-09-16 14:48:51 +0200

    Improved calendar view and added previous/next month buttons

--- Doctor/scripts/calendar.js
+++ Doctor/scripts/calendar.js
@@ -1,4 +1,6 @@
 
+let calendarShow = 0;
+
 function settingDate(date, day) {
     date = new Date(date);
     date.setDate(day);
@@ -30,9 +32,9 @@ function getDatesBetween(startDate, endDate) {
             startDate.setMonth(startDate.getMonth() + 1);
         }
     }
-    console.log(dates);
+    // console.log(dates);
 
-    let content = "";
+    let content = "<div class='calendarBtn'><button id='calendarPrev' onclick='prevMonth()' disabled>Prev</button> | <button id='calendarNext' onclick='nextMonth()'>Next</button></div>";
     let weekDays = [{shortDay:"Mon", fullDay:"Monday"}, {shortDay:"Tue", fullDay:"Tuesday"},
         {shortDay:"Wed", fullDay:"Wednesday"}, {shortDay:"Thu", fullDay:"Thursday"}, {shortDay:"Fri", fullDay:"Friday"},
         {shortDay:"Sat", fullDay:"Saturday"}, {shortDay:"Sun", fullDay:"Sunday"}];
@@ -41,9 +43,9 @@ function getDatesBetween(startDate, endDate) {
     for (let i = 0; i < dates.length; i++) {
         lastDate = dates[i];
         firstDate = new Date(lastDate.getFullYear(), lastDate.getMonth(), 1);
-        content += "<div id='calendarTable_" + (i+1) + "'>";
+        content += "<div id='calendarTable_" + (i+1) + "' class='calendarDiv'>";
         content += "<h2>" + firstDate.toString().split(" ")[1] + "-" + firstDate.getFullYear() + "</h2>";
-        content += "<table >";
+        content += "<table class='calendarTable'>";
         content += "<thead >";
         weekDays.map(item=>{
             content += "<th>" + item.fullDay + "</th>";
@@ -79,6 +81,35 @@ function getDatesBetween(startDate, endDate) {
         content += "</div>";
     }
     return content;
+}
+
+function prevMonth() {
+    let allMonths = document.getElementsByClassName("calendarDiv");
+    document.getElementById("calendarNext").disabled = false;
+    allMonths[calendarShow].style.display = "none";
+    calendarShow--;
+    if (calendarShow >= 0) {
+        allMonths[calendarShow].style.display = "block";
+        if (calendarShow === 0) {
+            document.getElementById("calendarPrev").disabled = true;
+        }
+    }
+}
+
+function  nextMonth() {
+    let allMonths = document.getElementsByClassName("calendarDiv");
+    document.getElementById("calendarPrev").disabled = false;
+    allMonths[calendarShow].style.display = "none";
+    calendarShow++;
+    if (calendarShow < allMonths.length) {
+        allMonths[calendarShow].style.display = "block";
+        if (calendarShow === allMonths.length - 1) {
+            document.getElementById("calendarNext").disabled = true;
+        }
+    }
+    // for (let i = 0; i < allMonths.length; i++) {
+    //     allMonths[i].style.display = "none";
+    // }
 }
 
 
