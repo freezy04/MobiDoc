0bf7ccef76be5f32d6b2339bd7871e8610495dd9
gabe <gabelancaster0@gmail.com>
2021-09-17 12:52:29 +0200

    Calendar: available slots now displayed form firebase

--- Doctor/scripts/calendar.js
+++ Doctor/scripts/calendar.js
@@ -1,45 +1,55 @@
 
-var  events = [];
-//var events2 = [];
+// let  events = [];
+//let events2 = [];
 
+let eventsDict = {};
 let times = ["06:00", "06:30", "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30",
     "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30",
     "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00"];
 let weekDays = [{shortDay:"Mon", fullDay:"Monday"}, {shortDay:"Tue", fullDay:"Tuesday"},
     {shortDay:"Wed", fullDay:"Wednesday"}, {shortDay:"Thu", fullDay:"Thursday"}, {shortDay:"Fri", fullDay:"Friday"},
     {shortDay:"Sat", fullDay:"Saturday"}, {shortDay:"Sun", fullDay:"Sunday"}];
+// let userUID;
 
-const getDoctorList = () => {
+const getDoctorList = (docID) => {
     let database = firebase.database();
     let ref = database.ref().child('DocSetADT');
     ref.orderByKey().once("value",snapshot => {
         if (snapshot.exists()) {
             snapshot.forEach(function (childSnapshot) {
-                let doc_id = childSnapshot.key;
                 let app = childSnapshot.val();
-                let docUid = app.doctorUid
-                let docName = app.doctorName
-                let DocSurname = app.doctorSurname
-                let docSpec = app. doctorSpecialization
-                let docAD = app.doctorAvailableDay
-                let docSDate = app.doctorStartDate
-                let docEDate = app.doctorEndDate
-                let docStartTime = app.doctorAvailableStartTime
-                let docEndTime = app.doctorAvailableEndTime
-                let docApptInterval = app.doctorAppointmentInterval
-                var Allouputs =docUid +"," + docName +","+ DocSurname +","+ docSpec +","+ docAD +","+ docSDate +"," + docEDate +"," + docStartTime+"," + docEndTime+"," + docApptInterval;
+                // let docUid = app.doctorUid;
+                let docUid = app.doctorUID;
+                if (docUid === docID) {
+                    // let doc_id = childSnapshot.key;
 
-                events.push({docUid,docName,DocSurname,docSpec,docAD,docSDate,docEDate,docStartTime,docEndTime,docApptInterval}); // opt1
-                //events2.push(Allouputs); // opt2
-                // opt1 uses a array with sets within and those sets are comprised of the doctors details
-                // opt2 uses concatenations of all doctors information
+                    // let docName = app.doctorName;
+                    // let DocSurname = app.doctorSurname;
+                    // let docSpec = app. doctorSpecialization;
+                    // let docAD = app.doctorAvailableDay;
+                    // let docSDate = app.doctorStartDate;
+                    // let docEDate = app.doctorEndDate;
+                    // let docStartTime = app.doctorAvailableStartTime;
+                    // let docEndTime = app.doctorAvailableEndTime;
+                    // let docApptInterval = app.doctorAppointmentInterval;
+                    // let Allouputs =docUid +"," + docName +","+ DocSurname +","+ docSpec +","+ docAD +","+ docSDate +"," + docEDate +"," + docStartTime+"," + docEndTime+"," + docApptInterval;
+
+                    eventsDict[app.date] = app.slots;
+
+                    // events.push({docUid,docName,DocSurname,docSpec,docAD,docSDate,docEDate,docStartTime,docEndTime,docApptInterval}); // opt1
+                    //events2.push(Allouputs); // opt2
+                    // opt1 uses a array with sets within and those sets are comprised of the doctors details
+                    // opt2 uses concatenations of all doctors information
+                }
             });
-            console.log(events)
-            //console.log(events2)
+            console.log(eventsDict);
+            //console.log(events2);
         }
+        //todo: Gabe - auto manage dates
+        let content = getDatesBetween("01/01/2021", "01/01/2022");
+        document.getElementById("calendar").innerHTML = content;
     });
 
-    //todo: change to take docID as parameter, only return that doc's intervals
 }
 
 
@@ -125,12 +135,11 @@ function getDatesBetween(startDate, endDate) {
     return content;
 }
 
-//todo: Gabe - function to fill in all availability details from firebase
-
-function changeSlotAvailability(slotID) {//doctory func
+function changeSlotAvailability(slotID) {//doctor func
     let slot = document.getElementById(slotID)
     if (window.getComputedStyle(slot).backgroundColor === "rgb(240, 128, 128)") {//use firebase logic instead?
-        slot.style.backgroundColor = "green";
+        slot.style.backgroundColor = "greenyellow";
+
     } else {
         slot.style.backgroundColor = "lightcoral";
     }
@@ -143,11 +152,15 @@ function bookAppointmentPopup(slotID) {//patient func
 
 function openDayPopup(dayID) {
     // document.getElementById(dayID).style.backgroundColor = "green";
-    popup.style.display = "block";
+
+    let slots = eventsDict[dayID];
+
+    document.getElementById("dayPopup").style.display = "block";
     document.getElementById("dayPopupHeader").innerText = dayID;
     let content = "<tbody>";
     for (let i = 0; i < 28; i++) {
-        content += "<tr><td id='" + (i+1) + dayID + "' onclick='changeSlotAvailability(this.id)'>" + times[i] + " - " + times[i+1] + "</td></tr>";
+        let slotColour = (slots !== undefined && slots.charAt(i) === '1') ? "style='background-color: greenyellow'" : "";
+        content += "<tr><td id='" + (i+1) + dayID + "' onclick='changeSlotAvailability(this.id)'" + slotColour + ">" + times[i] + " - " + times[i+1] + "</td></tr>";
     }
     content += "</tbody>";
     document.getElementById("dayTable").innerHTML = content;
@@ -179,17 +192,27 @@ function  nextMonth() {
     }
 }
 
-let popup = document.getElementById("dayPopup");
-let popupClose = document.getElementsByClassName("close")[0];
-popupClose.onclick = function() {
-    popup.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target === popup) {
+
+function popupInit() {
+    let popup = document.getElementById("dayPopup");
+    let popupClose = document.getElementsByClassName("close")[0];
+    popupClose.onclick = function() {
         popup.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target === popup) {
+            popup.style.display = "none";
+        }
     }
 }
 
-//todo: Gabe - auto manage dates
-let content = getDatesBetween("01/01/2021", "01/01/2022");
-document.getElementById("calendar").innerHTML = content;
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        popupInit();
+        // userUID = user.uid;
+        getDoctorList(user.uid);
+    } else {
+        window.location.href = "../index.html"; // redirects the user to the log in page
+    }
+});
+// getDoctorList("4CTDmVZnTnRpRqqQGEn684O8o102");
